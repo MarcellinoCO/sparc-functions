dee8039a6902127b1ed394457a793a2e5fc35d8c
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-10-08 23:08:07 +0700

    refactor: update smoke dispresion algorithm

--- simulate-smoke/main.py
+++ simulate-smoke/main.py
@@ -2,13 +2,12 @@ import functions_framework
 from google.cloud import storage
 
 import pandas as pd
-from scipy.spatial import KDTree
+import numpy as np
 
 import re
 import json
 import urllib.request
 import subprocess
-import math
 
 
 def fetch_content(url):
@@ -32,7 +31,7 @@ def fetch_fire_data():
     url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_SouthEast_Asia_24h.csv"
 
     df = pd.read_csv(url)
-    df.head()
+    df = df[df["confidence"] == "high"].reset_index(drop=True)
 
     # Generate datetime column
     df["acq_datetime"] = pd.to_datetime(
@@ -103,7 +102,7 @@ def fetch_wind_data():
 
     # Download GFS file
     file_content = fetch_file(nomads_url)
-    with open(filename, "wb") as file:
+    with open("wind.grb", "wb") as file:
         file.write(file_content)
 
     # Run Grib2Json conversion
@@ -113,7 +112,7 @@ def fetch_wind_data():
         "--output", "wind.json",
         "--names",
         "--compact",
-        filename
+        "wind.grb"
     ]
 
     subprocess.run(command, capture_output=True, text=True)
@@ -126,69 +125,104 @@ def fetch_wind_data():
     blob.upload_from_filename("wind.json")
 
 
+def compute_zones(yellow_zone_extension=0.5):
+    # Load fire and wind data
+    with open("fire.json", "r") as file:
+        fire_data = json.load(file)
+
+    with open("wind.json", "r") as file:
+        wind_data = json.load(file)
+
+    # Extracting U-component, V-component, latitude, and longitude grids
+    u_component_data = wind_data[0]['data']
+    v_component_data = wind_data[1]['data']
+
+    nx = wind_data[0]['header']['nx']
+    ny = wind_data[0]['header']['ny']
+
+    latitude_grid = np.linspace(wind_data[0]['header']['la1'], wind_data[0]['header']['la2'], ny)
+    longitude_grid = np.linspace(wind_data[0]['header']['lo1'], wind_data[0]['header']['lo2'], nx)
+
+    u_component_grid = np.array(u_component_data).reshape(ny, nx)
+    v_component_grid = np.array(v_component_data).reshape(ny, nx)
+    
+    # Extracting fire coordinates and fire intensities
+    fire_coords = [(event['latitude'], event['longitude'], event['intensity']) for event in fire_data]
+    max_frp = max([coord[2] for coord in fire_coords])
+    
+    # Function to find the nearest grid point for given coordinates
+    def nearest_grid_point(lat, lon, grid_lats, grid_lons):
+        lat_idx = np.argmin(np.abs(grid_lats - lat))
+        lon_idx = np.argmin(np.abs(grid_lons - lon))
+
+        return lat_idx, lon_idx
+    
+    # Extract wind data for each fire coordinate
+    fire_wind_data = []
+    for coord in fire_coords:
+        lat, lon, frp = coord
+        normalized_frp = frp / max_frp
+
+        lat_idx, lon_idx = nearest_grid_point(lat, lon, latitude_grid, longitude_grid)
+
+        u_wind = u_component_grid[lat_idx, lon_idx] * (1 + normalized_frp)
+        v_wind = v_component_grid[lat_idx, lon_idx] * (1 + normalized_frp)
+
+        fire_wind_data.append({
+            "lat": lat,
+            "lon": lon,
+            "u_wind": u_wind,
+            "v_wind": v_wind
+        })
+    
+    # Compute new coordinates and areas based on wind data
+    def compute_dispersed_coords_and_areas(lat, lon, u_wind, v_wind, scaling_factor=1.0):
+        R = 6371.0  # Earth's radius in km
+
+        dispersal_distance = np.sqrt(u_wind**2 + v_wind**2) * scaling_factor
+        red_zone_area = np.pi * dispersal_distance**2
+
+        yellow_zone_distance = dispersal_distance * (1 + yellow_zone_extension)
+        yellow_zone_area = np.pi * (yellow_zone_distance**2 - dispersal_distance**2)
+        
+        delta_lat = (v_wind * scaling_factor) / R * (180.0 / np.pi)
+        delta_lon = (u_wind * scaling_factor) / (R * np.cos(np.pi * lat / 180.0)) * (180.0 / np.pi)
+
+        new_lat = lat + delta_lat
+        new_lon = lon + delta_lon
+
+        return new_lat, new_lon, red_zone_area, yellow_zone_area
+
+    # Compute dispersed coordinates and areas for each fire location
+    dispersed_data_with_areas = []
+    for entry in fire_wind_data:
+        lat, lon, u_wind, v_wind = entry['lat'], entry['lon'], entry['u_wind'], entry['v_wind']
+
+        red_lat, red_lon, red_area, yellow_area = compute_dispersed_coords_and_areas(lat, lon, u_wind, v_wind)
+        yellow_lat, yellow_lon = compute_dispersed_coords_and_areas(lat, lon, u_wind * (1 + yellow_zone_extension), v_wind * (1 + yellow_zone_extension))[:2]
+        
+        dispersed_data_with_areas.append({
+            "lat": lat,
+            "lon": lon,
+            "red_lat": red_lat,
+            "red_lon": red_lon,
+            "red_area": red_area,
+            "yellow_lat": yellow_lat,
+            "yellow_lon": yellow_lon,
+            "yellow_area": yellow_area
+        })
+
+    return dispersed_data_with_areas
+
+
 @functions_framework.cloud_event
 def simulate_smoke(_):
     fetch_fire_data()
     fetch_wind_data()
-
-    with open("fire.json", "r") as fire_file:
-        fire_data = json.load(fire_file)
-
-    with open("wind.json", "r") as wind_file:
-        wind_data = json.load(wind_file)
-
-    def generate_lat_lon_pairs(header):
-        latitudes, longitudes = [], []
-        lo1, la1 = header['lo1'], header['la1']
-        dx, dy = header['dx'], header['dy']
-        nx, ny = header['nx'], header['ny']
-        
-        for i in range(ny):
-            for j in range(nx):
-                longitudes.append(lo1 + j * dx)
-                latitudes.append(la1 + i * dy)
-                
-        return latitudes, longitudes
-    
-    flat_wind_coords, flat_wind_directions = [], []
-    for entry in wind_data:
-        latitudes, longitudes = generate_lat_lon_pairs(entry['header'])
-        flat_wind_coords.extend(zip(latitudes, longitudes))
-        flat_wind_directions.extend(entry['data'])
-
-    wind_tree = KDTree(flat_wind_coords)
-
-    def get_nearest_wind_direction(lat, lon):
-        _, idx = wind_tree.query([lat, lon])
-        return flat_wind_directions[idx]
-    
-    def generate_smoke_polygon(lat, lon, wind_direction, distance=1.0, width=0.01):
-        angle_rad = math.radians(wind_direction)
-        d_lat = distance * math.cos(angle_rad)
-        d_lon = distance * math.sin(angle_rad)
-        
-        # Calculate perpendicular direction for width
-        perp_angle_rad = angle_rad + math.pi / 2
-        w_lat = width * math.cos(perp_angle_rad)
-        w_lon = width * math.sin(perp_angle_rad)
-        
-        # Create polygon vertices in clockwise order
-        p1 = (lat + w_lat, lon + w_lon)
-        p2 = (lat - w_lat, lon - w_lon)
-        p3 = (lat + d_lat - w_lat, lon + d_lon - w_lon)
-        p4 = (lat + d_lat + w_lat, lon + d_lon + w_lon)
-        
-        return [p1, p2, p3, p4, p1]
-    
-    smoke_polygons = []
-    for fire_point in fire_data:
-        lat, lon = fire_point['latitude'], fire_point['longitude']
-        wind_dir = get_nearest_wind_direction(lat, lon)
-        smoke_polygon = generate_smoke_polygon(lat, lon, wind_dir)
-        smoke_polygons.append(smoke_polygon)
+    smokes = compute_zones()
 
     with open("smoke.json", "w") as file:
-        json.dump(smoke_polygons, file)
+        json.dump(smokes, file)
 
     # Upload json to cloud storage
     storage_client = storage.Client()
